6e1b46c287ba270c433890af8447ff8f7480f602
Angelica Perez <aperez9423@gmail.com>
2020-06-26 21:46:17 -0400

    got api request to work and began styling components work.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,27 @@
-// Write your Character component here
+import React from "react";
+import styled from 'styled-components';
+
+const Container = styled.div`
+    background: white;
+    margin: 1rem;
+    border: solid .2rem black;
+    width: 18rem;
+    display: flex;
+    flex-direction: column;
+`
+const CharacterCard = ({ name, height, mass, hair_color, eye_color, birth_year, gender}) => {
+
+    return(
+        <Container>
+            <h1>{name}</h1>
+            <p>Height: {height} cm</p>
+            <p>Mass: {mass} kg</p>
+            <p>Hair Color: {hair_color}</p>
+            <p>Eye Color: {eye_color}</p>
+            <p>Birth Year: {birth_year}</p>
+            <p>Gender: {gender}</p>
+        </Container>
+    );
+};
+
+export default CharacterCard;
